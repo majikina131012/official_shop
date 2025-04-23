13dd49a9d740e040ac45c6570af3c827dee7ff49
EC2 Default User <ec2-user@ip-172-31-0-78.ap-northeast-1.compute.internal>
2025-04-23 06:17:52 +0000

    apply bootstrap

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -47,7 +47,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # protected
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :last_name_kana, :postal_code, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :last_name_kana, :postal_code, :address, :email])
   end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
