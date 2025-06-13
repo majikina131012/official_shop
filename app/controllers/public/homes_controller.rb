162bf4cf6924d68dd288fb1a2d74a28824869e50
EC2 Default User <ec2-user@ip-172-31-0-78.ap-northeast-1.compute.internal>
2025-06-13 01:05:35 +0000

    viewを見やすくするためにページネーションの実装、ページ遷移がgoodsとcdでおなじになる問題発生

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,9 @@
 class Public::HomesController < ApplicationController
   
   def top
-    @goods = Item.joins(:genre).where(genres: {name: 'GOODS'})
-    @CDs = Item.joins(:genre).where(genres: {name: 'CD'})
-    @informations = Information.all
+    @goods = Item.joins(:genre).where(genres: {name: 'GOODS'}).page(params[:page]).per(8)
+    @CDs = Item.joins(:genre).where(genres: {name: 'CD'}).page(params[:page]).per(8)
+    @informations = Information.page(params[:page]).per(4)
   end
   
   def guide
